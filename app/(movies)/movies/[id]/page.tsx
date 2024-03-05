abd4b8bc6acb2d025ce1f34a08207a7bfa57fd30
stegano9744 <stegano9744@gmail.com>
2024-03-05 21:24:21 +0900

    ✨ Feat #11: Suspense
    Content:
    - page.tsx에 있는 두 가지 data fetching을 components 폴더에 각각 movie-info.tsx, movie-info.tsx 파일로 분리했다.
    - 각각의 파일들은 async await 컴포넌트를 가지고 있고 data fetching을 한다.
    - page.tsx 파일은 이 컴포넌트들을 불러와 Suspense 컴포넌트로 감싸서 data fetching을 병렬처리 했다.
    - Suspense 컴포넌트의 fallback prop은 data fetching이 일어나는 동안 보여줄 컴포넌트이다.
    - loading.tsx: 전체 페이지를 로딩 상태로 대체
    - Suspense Component: fetch해야 하는 component만 로딩 상태로 대체

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,20 +1,6 @@
-import { API_URL } from '../../../(home)/page';
-
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
+import { Suspense } from 'react';
+import MovieInfo from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
 
 // const ComponentName = async()=> {} 형식으로도 작성 가능
 export default async function MovieDetail({
@@ -22,12 +8,14 @@ export default async function MovieDetail({
 }: {
   params: { id: string };
 }) {
-  console.log('start fetching');
-  const [movies, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  console.log('end fetching');
   return (
     <div>
-      <h1>{movies.title}</h1>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
     </div>
   );
 }
